add89ddb17c41c95a393ff0fd8d88e8245960e5b
sgoyal3012@gmail.com <shivanig@247around.com>
2023-06-16 15:32:12 +0530

    functionality of insta like

---
+++ instalike/script.js
@@ -0,0 +1,22 @@
+const clickArea = document.querySelector(".inner-div");
+const clickCount = document.querySelector(".clickCount");
+
+let likeCount = 0;
+
+{/* <i class="fa-sharp fa-solid fa-heart"></i> */}
+const showHeart = (e) => {
+    const heart = document.createElement('i');
+    heart.classList.add('fas');
+    heart.classList.add('fa-heart');
+    clickArea.appendChild(heart);
+
+
+    setTimeout(() => {
+        heart.remove();
+    }, 800);
+};
+
+clickArea.addEventListener('dblclick' , (e) =>{
+    clickCount.innerHTML = ++likeCount;
+    showHeart(e);
+})
